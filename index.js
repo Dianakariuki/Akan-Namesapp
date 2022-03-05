36326c75f7fd531374e3570c689a694186eedd11
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-05 13:55:40 +0300

    This commit makes variabbles for the year,month,date and converts to a number

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function akan(){
-    
+    var year = parseFloat(document.getElementById("Year").value);
+    //console.log(year);
+  
+    var month = parseFloat(document.getElementById("Month").value);
+    //console.log(month);
+  
+    var day = parseFloat(document.getElementById("Date").value);
+    //console.log(day)
 }
