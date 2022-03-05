4ebcd21be93900628066dda4f25fa33d9a4acf6c
Diana Kariuki <dianakariuki842@gmail.com>
2022-03-05 14:08:02 +0300

    This commit creates  output for results using the if statements

--- index.js
+++ index.js
@@ -13,4 +13,55 @@ function akan(){
     var gender = document.getElementById("gender").value;
     //console.log(gender);
 
+
+    var male= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    
+    var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa", "Afua","Ama"];
+
+    {
+        if(birthday==0 && gender=="male") {
+          document.getElementById("results").innerHTML = male [0];
+        }
+        if(birthday==1 && gender=="male") {
+          document.getElementById("results").innerHTML = male [1];
+        }
+        if(birthday==2 && gender=="male") {
+          document.getElementById("results").innerHTML = male [2];
+        }
+        if(birthday==3 && gender=="male") {
+          document.getElementById("results").innerHTML = male [3];
+        }
+        if(birthday==4 && gender=="male") {
+          document.getElementById("results").innerHTML = male [4];
+        }
+        if(birthday==5 && gender=="male") {
+          document.getElementById("results").innerHTML = male [5];
+        }
+        if(birthday==6 && gender=="male") {
+          document.getElementById("results").innerHTML = male [6];
+        }
+
+
+        if(birthday==0 && gender=="female") {
+          document.getElementById("results").innerHTML = female [0];
+        }
+        if(birthday==1 && gender=="female") {
+          document.getElementById("results").innerHTML = female [1];
+        } 
+        if(birthday==2 && gender=="female") {
+          document.getElementById("results").innerHTML = female [2];
+        } 
+        if(birthday==3 && gender=="female") {
+          document.getElementById("results").innerHTML = female [3];
+        } 
+        if(birthday==4 && gender=="female") {
+          document.getElementById("results").innerHTML = female [4];
+        } 
+        if(birthday==5 && gender=="female") {
+          document.getElementById("results").innerHTML = female [5];
+        } 
+        if (birthday==6 && gender=="female") {
+          document.getElementById("results").innerHTML = female [6];
+        }  
+    }
 }
